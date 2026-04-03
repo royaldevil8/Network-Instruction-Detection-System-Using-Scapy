f62f023890576076922e305202eb0044e32b3ade
Ankit Rajput <ar9260772@gmail.com>
2026-04-03 21:00:13 +0530

    Delete sniffer.py

--- sniffer.py
+++
@@ -1,9 +0,0 @@
-from scapy.all import sniff, IP, TCP
-from detector import analyze_packet
-
-def process_packet(pkt):
-    if pkt.haslayer(IP):
-        analyze_packet(pkt)
-
-def start_sniffing():
-    sniff(prn=process_packet, store=False)
